6069e2ce31b1d20994528fb1d72bd140c8088363
KristhDev <kafr9102@gmail.com>
2021-09-11 11:05:31 -0600

    Se creo la migration para la table orders, junto a su controller

---
+++ migrations/m210911_164300_create_orders_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use yii\db\Migration;
+
+/**
+ * Handles the creation of table `{{%orders}}`.
+ */
+class m210911_164300_create_orders_table extends Migration
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function safeUp()
+    {
+        $this->createTable('{{%orders}}', [
+            'id' => $this->primaryKey(),
+            'user_id' => $this->integer(255)->notNull(),
+            'product_id' => $this->integer(255)->notNull(),
+            'direction' => $this->text()->notNull(),
+            'quantity' => $this->integer(255)->notNull(),
+            'total_to_pay' => $this->integer(255)->notNull(),
+            'status' => "ENUM('Pendiente', 'Cancelado', 'Confirmado')",
+            'date' => $this->dateTime()->notNull(),
+            'date_confirm' => $this->dateTime()->notNull()
+        ]);
+
+        $this->addForeignKey('fk_orders_user', '{{%orders}}', 'user_id', '{{%users}}', 'id');
+        $this->addForeignKey('fk_orders_product', '{{%orders}}', 'product_id', '{{%products}}', 'id');
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function safeDown()
+    {
+        $this->dropForeignKey('fk_orders_user', '{{%orders}}');
+        $this->dropForeignKey('fk_orders_product', '{{%orders}}');
+        $this->dropTable('{{%orders}}');
+    }
+}
